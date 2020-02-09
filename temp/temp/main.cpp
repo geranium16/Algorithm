22da574faa7ab3e8bc776790bd8c4d83d4f897ad
DaeHyeon Park <geranium16@naver.com>
2020-02-09 22:19:54 +0900

    2020/02/09

--- temp/temp/main.cpp
+++ temp/temp/main.cpp
@@ -1,60 +1,64 @@
 #include <iostream>
-#include <algorithm>
 #include <vector>
 using namespace std;
-#define MAX 18
-int n;
-int mymap[MAX][MAX];
-bool visited[10];
-int sol=987987;
-int cal(vector <int>& team){
+#define MAX 12
+
+int n,m,c;
+int mymap[MAX][MAX]={0,};
+int mydata[MAX][MAX]={0,};
+
+
+
+//최대 채취량을 구하는 로직 : n개의 배열중 모든 가능한 조합 도출
+int cal(vector <int>& v,int idx,int sum,int real){
+    if(sum>c) return 0; //최대채취량보다 크면 종료
+    if(idx==v.size()) return real; //벌통채취완료시 종류
+    return max(cal(v,idx+1,sum+v[idx],real+v[idx]*v[idx]),cal(v,idx+1,sum,real));
+}
+//각 노드를 시작점으로 하는 최대 채취량
+void getMax(int x,int y){
+    
+    vector <int> v;
+    for(int i=0;i<m;i++)
+        v.push_back(mymap[x][y+i]);
+    mydata[x][y]=cal(v,0,0,0);
+}
+int dfs(int x,int y){
+    //같은 라인
     int ans=0;
-    for(int i=0;i<n/2;i++){
-        for(int j=0;j<n/2;j++)
-            ans+=mymap[team[i]][team[j]];
-        
+    for(int i=y+m;i<n-m+1;i++){
+        ans=max(ans,mydata[x][i]);
+    }
+    for(int i=x+1;i<n;i++){
+        for(int j=0;j<n-m+1;j++)
+            ans=max(ans,mydata[i][j]);
     }
     return ans;
-}
-void combination(){
-    vector <int> a;
-    vector <int> b;
-    for(int i=0;i<n;i++){
-        
-        if(visited[i])
-            a.push_back(i);
-        else
-            b.push_back(i);
-    }
-    int result=abs(cal(a)-cal(b));
-    if(sol>result)
-        sol=result;
-    
-}
-void DFS(int idx,int cnt){
-    
-    if(cnt>=n/2){
-        combination();
-        return;
-    }
-    for(int i=idx;i<n;i++){
-        visited[i]=true;
-        DFS(i+1,cnt+1);
-        visited[i]=false;
-    }
 }
 int main(){
     int tc;
     cin>>tc;
     for(int t=1;t<=tc;t++){
-        sol=987987987;
-        cin>>n;
+        cin>>n>>m>>c;
         for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++)
+            for(int j=0;j<n;j++){
                 cin>>mymap[i][j];
+            }
         }
-        DFS(0,0);
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n-m+1;j++){
+                getMax(i,j);
+            }
+        }
+        //i,j를 선택한 상태에서 찾을수 있는 나머지에 대해
+        //비교하여 최대값 찾는다.
+        int sol = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n - m + 1; j++) {
+                sol = max(sol, dfs(i, j)+mydata[i][j]);
+            }
+        }
+        
         cout<<"#"<<t<<" "<<sol<<"\n";
     }
-
 }
