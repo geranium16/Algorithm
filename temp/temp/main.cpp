9105a8068fc6ee2622aff14db22fffd2da2d76b8
DaeHyeon Park <geranium16@naver.com>
2020-03-19 12:28:59 +0900

    2020/03/19

--- temp/temp/main.cpp
+++ temp/temp/main.cpp
@@ -1,129 +1,104 @@
 #include <iostream>
 #include <vector>
-#include <queue>
 #include <cstring>
 using namespace std;
-const int MAX=17;
+const int MAX=55;
 
 
-int n,m,d;
-int enemy_num=0;
-int enemy_num_copy=0;
-int sol=0,ans=0;
-bool mymap[MAX][MAX];
-bool copymap[MAX][MAX];
-int dx[3]={0,-1,0};
-int dy[3]={-1,0,1};
+int mymap[MAX][MAX];
+int copy_map[MAX][MAX];
+int n,m,k; // k=회전 횟수
+vector <int> row; //row
+vector <int> col; //co
+vector <int> s; // 칸수
+vector <vector<int>> per;
+vector <int> temp;
 
-vector <pair<int,int>> hunter;
-vector <pair<int,int>> enemy;
-bool range(int x,int y){
-    return 0<=x && x<n && 0<=y && y<m;
+bool visited[7];
+int sol=987987987;
+int calculate(){
+    int sum=987987987;
+    for(int i=1;i<=n;i++){
+        int temp_sum=0;
+        for(int j=1;j<=m;j++){
+            temp_sum+=copy_map[i][j];
+            //cout<<copy_map[i][j]<<" ";
+        }
+    
+       // cout<<temp_sum<<"\n";
+        sum=min(sum,temp_sum);
+    }
+    return sum;
+}
+void simulation(int row_s, int row_e, int col_s, int col_e,int totalcnt,int cnt){
+    
+    if(cnt>=totalcnt)
+        return;
+        
+
+    int temp=copy_map[row_s][col_s];
+    for(int i=row_s;i<row_e;i++) // col=col_s 고정
+        copy_map[i][col_s] = copy_map[i+1][col_s];
+    
+    for(int i=col_s;i<col_e;i++) // row=row_e-1 고정
+        copy_map[row_e][i]=copy_map[row_e][i+1];
+    
+    for(int i=row_e;i>row_s;i--) // col=col_e-1 고정
+        copy_map[i][col_e]=copy_map[i-1][col_e];
+    
+    for(int i=col_e;i>col_s;i--) // row=row_s 고정
+        copy_map[row_s][i]=copy_map[row_s][i-1];
+    copy_map[row_s][col_s+1]=temp;
+    
+    simulation(row_s+1,row_e-1,col_s+1,col_e-1,totalcnt,cnt+1);
 }
 
-void findenemy(int x,int y){
-    queue <pair<int,int>> q;
-    bool visited[MAX][MAX]={false,};
-    if(copymap[x-1][y]){
-        enemy.push_back({x-1,y});
+void permutation(int cnt){
+    if(cnt>=k){
+        per.push_back(temp);
         return;
     }
-    q.push({x-1,y});
-  
-    while(!q.empty()){
-        pair<int,int> current=q.front();
-        q.pop();
-        for(int i=0;i<3;i++){
-            pair<int,int> next={current.first+dx[i],current.second+dy[i]};
-            if(abs(next.first-x)+abs(next.second-y)>d)
-                return;
-            if(range(next.first,next.second)){
-                if(copymap[next.first][next.second]){
-                    enemy.push_back(next);
-                    return;
-                }
-                else{
-                    visited[next.first][next.second]=true;
-                    q.push(next);
-                }
-            }
+    for(int i=0;i<k;i++){
+        if(!visited[i]){
+            visited[i]=true;
+            temp.push_back(i);
+            permutation(cnt+1);
+            temp.pop_back();
+            visited[i]=false;
         }
-    }
-}
-void move(){
-    for(int i=0;i<m;i++){
-        if(copymap[n-1][i]){
-            copymap[n-1][i]=false;
-            enemy_num_copy--;
-        }
-    }
-    for(int i=n-2;i>=0;i--){
-        for(int j=0;j<m;j++){
-            if(copymap[i][j]){
-                copymap[i][j]=false;
-                copymap[i+1][j]=true;
-            }
-        }
-    }
-}
-void simulation(){
-    if(enemy_num_copy<=0){
-        return;
-    }
-    enemy.clear();
-    for(int i=0;i<3;i++){
-        findenemy(hunter[i].first,hunter[i].second);
-    }
-    
-    for(int i=0;i<enemy.size();i++){
-        if(copymap[enemy[i].first][enemy[i].second]){
-            copymap[enemy[i].first][enemy[i].second]=false;
-            enemy_num_copy--;
-            ans++;
-        }
-    }
-    
-//    if(hunter[0].second==0 && hunter[1].second==2 && hunter[2].second==4){
-//        for(int i=0;i<n;i++){
-//            for(int j=0;j<m;j++){
-//                cout<<copymap[i][j]<<" ";
-//            }
-//            cout<<"\n";
-//        }
-//        cout<<"\n";
-//    }
-    move();
-    simulation();
-}
-void solution(int cnt,int idx){
-    if(cnt>=3){
-        memcpy(copymap,mymap,sizeof(mymap));
-        enemy_num_copy=enemy_num;
-        ans=0;
-//        for(int i=0;i<3;i++){
-//            cout<<hunter[i].second<<" ";
-//        }
-//        cout<<"\n";
-        simulation();
-        if(ans>sol)
-            sol=ans;
-    }
-    for(int i=idx;i<m;i++){
-        hunter.push_back({n,i});
-        solution(cnt+1,i+1);
-        hunter.pop_back();
     }
 }
 
 int main(){
-    cin>>n>>m>>d;
-    for(int i=0;i<n;i++){
-        for(int j=0;j<m;j++){
-            cin>>mymap[i][j];
-            if(mymap[i][j])
-                enemy_num++;
-        }
+    cin>>n>>m>>k;
+    for(int i=0;i<k;i++){
+        vector<int> element;
+        
     }
-    solution(0,0);
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=m;j++)
+            cin>>mymap[i][j];
+    }
+    
+    for(int i=0;i<k;i++){
+        int a,b,c;
+        cin>>a>>b>>c;
+        row.push_back(a);
+        col.push_back(b);
+        s.push_back(c);
+    }
+    permutation(0);
+    
+    for(int j=0;j<per.size();j++){
+        memcpy(copy_map,mymap,sizeof(mymap));
+        for(int i=0;i<k;i++)
+          simulation(row[per[j][i]]-s[per[j][i]],row[per[j][i]]+s[per[j][i]],col[per[j][i]]-s[per[j][i]],col[per[j][i]]+s[per[j][i]],s[per[j][i]],0);
+        
+        int ans=calculate();
+        if(sol>ans)
+            sol=ans;
+    }
     cout<<sol;
+    
 }
+
